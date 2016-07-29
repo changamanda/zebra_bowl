099ed6c15e88d823295777fab94b992e086ea352
changamanda <amanda.chang10@gmail.com>
2016-07-29 17:03:07 -0500

    Add some inline documentation to models

--- app/models/frame.rb
+++ app/models/frame.rb
@@ -7,12 +7,15 @@ class Frame < ApplicationRecord
     self.frame_number = object[:frame_number]
   end
 
+  # Calls set_throw_value with the number of pins knocked down by the bowl
+  # Returns the frame referenced by self
   def bowl(pins)
     raise_bowl_errors(pins)
     set_throw_value(pins)
     self
   end
 
+  # Sets the appropriate throw attribute (:first_throw, :second_throw, or :third_throw) to pins
   def set_throw_value(pins)
     if two_throws?
       self.update(third_throw: pins)
@@ -23,18 +26,24 @@ class Frame < ApplicationRecord
     end
   end
 
+  # Returns the sum of all throws in the current frame (0 for nil)
   def pins_sum
     self.first_throw.to_i + self.second_throw.to_i + self.third_throw.to_i
   end
 
+  # Returns an array of all throws that have been set, in order
   def moves
     [first_throw, second_throw, third_throw].compact
   end
 
+  # Sets the frame's cumulative_score attribute to total_score
   def update_cumulative_score(total_score)
     self.update(cumulative_score: total_score)
   end
 
+  # Updates the score attribute of the frame, if possible
+  # If next_frame is nil, strike and spare scores will not be updated
+  # If next_next_frame is nil, double strike scores will not be updated
   def update_score(next_frame, next_next_frame)
     moves = Frame.get_next_moves(next_frame, next_next_frame)
     if tenth_frame?
@@ -53,28 +62,30 @@ class Frame < ApplicationRecord
     self.frame_number <=> other_frame.frame_number
   end
 
+  # Returns an array representation of the frame: 'X' for strikes and '/' for spares
   def to_a
     moves = self.moves
 
     if strike?
-      moves[0] = "X"
+      moves[0] = 'X'
     end
 
     if spare?
-      moves[1] = "/"
+      moves[1] = '/'
     elsif self.second_throw.to_i == 10
-      moves[1] = "X"
+      moves[1] = 'X'
     end
 
     if self.third_throw.to_i == 10
-      moves[2] = "X"
+      moves[2] = 'X'
     elsif (self.third_throw && (self.second_throw.to_i + self.third_throw.to_i == 10))
-      moves[2] = "/"
+      moves[2] = '/'
     end
 
     moves
   end
 
+  # Returns an array of all moves in next_frame and next_next_frame
   def self.get_next_moves(next_frame, next_next_frame)
     moves = []
     moves += next_frame.moves if next_frame
@@ -86,11 +97,11 @@ class Frame < ApplicationRecord
 
     def raise_bowl_errors(pins)
       if pins > 10
-        raise "Pins limit exceeded!"
+        raise 'Pins limit exceeded!'
       elsif !tenth_frame? && (pins_sum + pins) > 10
-        raise "Pins limit exceeded!"
+        raise 'Pins limit exceeded!'
       elsif complete?
-        raise "Throws already set."
+        raise 'Throws already set.'
       end
     end
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -9,6 +9,8 @@ class Player < ApplicationRecord
     self.frames << Frame.new({frame_number: self.frame_number})
   end
 
+  # Bowls for the current frame, and increments the current frame if necessary
+  # Returns the player referenced by self
   def bowl(pins)
     raise_bowl_errors
     current_frame.bowl(pins)
@@ -19,6 +21,8 @@ class Player < ApplicationRecord
     self
   end
 
+  # Iterates through frames and attempts to score all that haven't yet been scored
+  # Calls update_total_score for each newly scored frame
   def update_frame_scores
     sorted_frames = self.frames.sort
     sorted_frames.each_with_index do |frame, index|
@@ -29,6 +33,8 @@ class Player < ApplicationRecord
     end
   end
 
+  # Updates the total_score attribute by adding frame_score to the previous total
+  # Calls Frame#update_cumulative_score for frame
   def update_total_score(frame_score, frame)
     if frame_score
       self.update(total_score: total_score + frame_score)
